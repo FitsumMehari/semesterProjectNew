80d80f37c1917ba65c5593476ef09f904976661e
FitsumMehari <m.fitsummehari@gmail.com>
2024-08-19 18:48:26 +0300

    add icons

--- app/exitExam/src/app/services/exam.service.ts
+++ app/exitExam/src/app/services/exam.service.ts
@@ -9,14 +9,23 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class ExamService {
-  constructor(private http: HttpClient, private auth: AuthService, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
   _exams: BehaviorSubject<any> = new BehaviorSubject([]);
+  _exam: BehaviorSubject<any> = new BehaviorSubject([]);
+
   message!: string;
   response: any;
 
   setExams(exams: any) {
     this._exams.next(exams);
+  }
+  setExam(exam: any) {
+    this._exam.next(exam);
   }
 
   getExams(fieldofstudy: string) {
@@ -65,13 +74,13 @@ export class ExamService {
           score: `${score} / ${exam.questions.length}`,
         };
 
-
         const url = environment.apiURL + 'score/:' + user.id;
 
         const httpOptions = {
-          headers: new HttpHeaders({ 'Content-Type': 'application/json',
-            'token': `token ${localStorage.getItem('token')}`
-           }),
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            token: `token ${localStorage.getItem('token')}`,
+          }),
         };
 
         this.http.post(url, scoreToSave, httpOptions).subscribe(
@@ -81,8 +90,89 @@ export class ExamService {
           },
           (error) => {}
         );
-
       });
+    });
+  }
+
+  // For admin
+
+  getAllExams() {
+    const url = environment.apiURL + 'admin-exam/:all';
+
+    // const url = `http://localhost:3000/material/:${userId}`;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: `token ${localStorage.getItem('token')}`,
+      }),
+    };
+
+    return this.http.get(url, httpOptions).subscribe(
+      (next) => {
+        this.response = next;
+        this.message = this.response.message;
+
+        this.setExams(next);
+      },
+      (error) => {}
+    );
+  }
+
+  getSingleExam(examId: any) {
+
+
+    const url = environment.apiURL + 'admin-exam/:' + examId;
+
+    // const url = `http://localhost:3000/material/:${userId}`;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: `token ${localStorage.getItem('token')}`,
+      }),
+    };
+
+    return this.http.get(url, httpOptions).subscribe((next) => {
+      this.setExam(next);
+        });
+  }
+
+  addExam(material: any) {
+    const url = environment.apiURL + 'admin-exam/';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: `token ${localStorage.getItem('token')}`,
+      }),
+    };
+    return this.http.post(url, material, httpOptions).subscribe((next) => {
+      this.response = next;
+      alert(this.response.message);
+      this.router.navigate(['/admin/home/exams']);
+    });
+  }
+
+  deleteExam(examId: any) {
+    const url = environment.apiURL + 'admin-exam/:' + examId;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: `token ${localStorage.getItem('token')}`,
+      }),
+    };
+    return this.http.delete(url, httpOptions).subscribe((next) => {
+      this.response = next;
+      alert(this.response.message);
+      this.router.navigate(['/admin/home/exams']);
+    });
+  }
+
+  updateExam(exam: any) {
+    const url = environment.apiURL + 'admin-exam/';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        token: `token ${localStorage.getItem('token')}`,
+      }),
+    };
+    return this.http.put(url, exam, httpOptions).subscribe((next) => {
+      this.response = next;
+      alert(this.response.message);
+      this.router.navigate(['/admin/home/exams']);
     });
   }
 }
