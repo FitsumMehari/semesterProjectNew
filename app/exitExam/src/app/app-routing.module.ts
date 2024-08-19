d1a898d962f72e4d673e3d18a7867647a53711a2
FitsumMehari <m.fitsummehari@gmail.com>
2024-08-19 19:10:24 +0300

    add built app to api

--- app/exitExam/src/app/app-routing.module.ts
+++ app/exitExam/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {path: '', redirectTo: 'user', pathMatch: 'full'},
+  {path: '', redirectTo: 'user', pathMatch: 'full'},
   {path: 'user', loadChildren: ()=>import ("./user/user.module").then(m => m.UserModule)},
   {path: 'admin', loadChildren: ()=>import ("./admin/admin.module").then(m => m.AdminModule)}
 ];
