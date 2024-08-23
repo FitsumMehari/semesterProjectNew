cf681c2490342874e9616152232fd690f3cf7c80
FitsumMehari <m.fitsummehari@gmail.com>
2024-08-23 16:40:51 +0300

    dispay number of exams

--- app/exitExam/src/app/admin/pages/overview/overview.component.ts
+++ app/exitExam/src/app/admin/pages/overview/overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MaterialService } from '../../../services/material.service';
 import { UserService } from '../../../services/user.service';
+import { ExamService } from '../../../services/exam.service';
 
 @Component({
   selector: 'app-overview',
@@ -8,23 +9,34 @@ import { UserService } from '../../../services/user.service';
   styleUrl: './overview.component.css',
 })
 export class OverviewComponent implements OnInit {
-  constructor(private matService: MaterialService, private userService: UserService) {}
+  constructor(private matService: MaterialService, private userService: UserService, private examService: ExamService) {}
 
   users: any = []
   materials: any
+  exams: any
 
   ngOnInit(): void {
     this.refreshMaterialsList();
     this.refreshUsersList();
+    this.refreshExamsList();
 
   }
-   // Refresh The List
+   // Refresh The Lists
    refreshMaterialsList() {
     this.matService.getAllMaterials();
     this.matService._materials.subscribe((next) => {
       this.materials = next;
     });
   }
+
+
+   refreshExamsList() {
+    this.examService.getAllExams();
+    this.examService._exams.subscribe((next) => {
+      this.exams = next;
+    });
+  }
+
   refreshUsersList() {
     this.userService.getAllUsers();
     this.userService._users.subscribe((next) => {
