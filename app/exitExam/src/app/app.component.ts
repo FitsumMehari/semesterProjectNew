56c1054e4c5be099309f4dbb54d41e93688a1c70
FitsumMehari <m.fitsummehari@gmail.com>
2024-09-28 09:24:19 +0300

    use the selected language at root level

--- app/exitExam/src/app/app.component.ts
+++ app/exitExam/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private translateService: TranslateService) {
+    this.translateService.setDefaultLang('en');
+    this.translateService.use(localStorage.getItem('lang') || 'en')
+  }
+
   title = 'exitExam';
 }
