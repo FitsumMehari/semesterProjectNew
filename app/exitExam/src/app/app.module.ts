1f1fa0760e581bf34fa6a59b3acd70e08188d788
FitsumMehari <m.fitsummehari@gmail.com>
2024-09-28 09:24:45 +0300

    add the ngx-translate module forRoot

--- app/exitExam/src/app/app.module.ts
+++ app/exitExam/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
 import { FormsModule } from '@angular/forms';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 import { MaterialService } from './services/material.service';
 import { ResultService } from './services/result.service';
@@ -14,10 +14,39 @@ import { QuestionService } from './services/question.service';
 import { AuthGuardService } from './services/auth-guard.service';
 import { AdminGuardService } from './services/admin-guard.service';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient ) {
+  return new TranslateHttpLoader(http, 'i18n/', '.json')
+}
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, UserModule, AdminModule],
-  providers: [provideHttpClient(), AuthService, MaterialService, ResultService, QuestionService, AuthGuardService, AdminGuardService],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UserModule,
+    AdminModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+    },
+    defaultLanguage: 'en',
+    }),
+  ],
+  providers: [
+    provideHttpClient(),
+    AuthService,
+    MaterialService,
+    ResultService,
+    QuestionService,
+    AuthGuardService,
+    AdminGuardService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
