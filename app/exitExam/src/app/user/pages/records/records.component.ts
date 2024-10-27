99869452b10da7960577fde23827aa4195bb3aaf
FitsumMehari <m.fitsummehari@gmail.com>
2024-10-27 16:11:58 +0300

    get results for a user

--- app/exitExam/src/app/user/pages/records/records.component.ts
+++ app/exitExam/src/app/user/pages/records/records.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ResultService } from '../../../services/result.service';
+import { AuthService } from '../../../services/auth.service';
 @Component({
   selector: 'app-records',
   templateUrl: './records.component.html',
-  styleUrl: './records.component.css'
+  styleUrl: './records.component.css',
 })
-export class RecordsComponent {
+export class RecordsComponent implements OnInit {
+  constructor(private resultService: ResultService, private auth: AuthService) {}
 
+  results = [{
+    examTitle: '',
+    fieldofstudy: '',
+    score: '',
+    updatedAt:''
+  }];
+  userId :any;
+
+  ngOnInit(): void {
+    this.auth._user.subscribe((next) => {
+      this.userId = next._id;
+    })
+    this.resultService.getResultsForOneUser(this.userId)
+    this.resultService._results.subscribe((next) => {
+      this.results = next;
+      console.log(this.results);
+    });
+
+  }
 }
