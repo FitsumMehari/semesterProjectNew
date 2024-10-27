a3b2bd4466e593714986c7a3bc04fb5270967bff
FitsumMehari <m.fitsummehari@gmail.com>
2024-10-27 14:51:23 +0300

    intialize records component

---
+++ app/exitExam/src/app/user/pages/records/records.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-records',
+  templateUrl: './records.component.html',
+  styleUrl: './records.component.css'
+})
+export class RecordsComponent {
+
+}
