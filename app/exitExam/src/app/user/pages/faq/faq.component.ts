88768f8b92f48e9870f81802c597480a51748228
FitsumMehari <m.fitsummehari@gmail.com>
2024-10-27 18:05:23 +0300

    add faqs

--- app/exitExam/src/app/user/pages/faq/faq.component.ts
+++ app/exitExam/src/app/user/pages/faq/faq.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.css'
+  styleUrl: './faq.component.css',
 })
-export class FAQComponent {
-
+export class FAQComponent implements OnInit {
+  ngOnInit(): void {
+   
+  }
 }
